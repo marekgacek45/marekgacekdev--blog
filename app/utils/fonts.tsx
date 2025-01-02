ca2484ed7edd11de530a642aa1da291d87f887df
marekgacek45 <marekgacek45@gmail.com>
2025-01-02 16:15:27 +0100

    setup page and create navbar

---
+++ app/utils/fonts.tsx
@@ -0,0 +1,25 @@
+import { Russo_One, Roboto_Mono, VT323 } from 'next/font/google'
+
+export const font_heading_init = Russo_One({
+	subsets: ['latin'],
+	display: 'swap',
+	variable: '--font-heading',
+	weight: ['400'],
+})
+
+export const font_text_init = Roboto_Mono({
+	subsets: ['latin'],
+	display: 'swap',
+	variable: '--font-text',
+	weight: ['300', '400', '500', '600', '700'],
+})
+export const font_accent_init = VT323({
+	subsets: ['latin'],
+	display: 'swap',
+	variable: '--font-accent',
+	weight: ['400'],
+})
+
+export const font_heading = font_heading_init.variable
+export const font_text = font_text_init.variable
+export const font_accent = font_accent_init.variable
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	images: {
+		formats: ['image/avif', 'image/webp'],
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'cdn.sanity.io',
+			},
+		],
+	},
+}
 
-export default nextConfig;
+export default nextConfig
